93896e9a8d77b5d5558667bfd23296c3859663ec
Ethel Marquer <ethel.marquer@gmail.com>
2015-08-21 10:34:45 +0200

    Header definition

--- test_motors.cpp
+++ test_motors.cpp
@@ -2,94 +2,6 @@
 #include "test_motors.h"
 
 
-/* Define pinout of Teensy to match physical connections */
-#define PWMA 20
-#define AIN1 11
-#define AIN2 12
-#define PWMB 21
-#define BIN1 15
-#define BIN2 14
-// STBY not defined, always at high state.
-
-void goForward ()
-{
-	digitalWrite (AIN1,HIGH);
-	digitalWrite (AIN2,LOW);
-	analogWrite(PWMA,234);
-	digitalWrite (BIN1,HIGH);
-	digitalWrite (BIN2,LOW);
-	analogWrite(PWMB,255);
-}
-
-void goBackward ()
-{
-	digitalWrite (AIN1,LOW);
-	digitalWrite (AIN2,HIGH);
-	analogWrite(PWMA,233);
-	digitalWrite (BIN1,LOW);
-	digitalWrite (BIN2,HIGH);
-	analogWrite(PWMB,255);
-}
-
-void rotateRight ()
-{
-	digitalWrite (AIN1,HIGH);
-	digitalWrite (AIN2,LOW);
-	analogWrite(PWMA,255);
-	digitalWrite (BIN1,LOW);
-	digitalWrite (BIN2,HIGH);
-	analogWrite(PWMB,255);
-}
-
-void rotateLeft ()
-{
-	digitalWrite (AIN1,LOW);
-	digitalWrite (AIN2,HIGH);
-	analogWrite(PWMA,255);
-	digitalWrite (BIN1,HIGH);
-	digitalWrite (BIN2,LOW);
-	analogWrite(PWMB,255);
-}
-
-void veerLeft ()
-{
-	digitalWrite (AIN1,HIGH);
-	digitalWrite (AIN2,LOW);
-	analogWrite(PWMA,190);
-	digitalWrite (BIN1,HIGH);
-	digitalWrite (BIN2,LOW);
-	analogWrite(PWMB,255);
-}
-
-void veerRight ()
-{
-	digitalWrite (AIN1,HIGH);
-	digitalWrite (AIN2,LOW);
-	analogWrite(PWMA,255);
-	digitalWrite (BIN1,HIGH);
-	digitalWrite (BIN2,LOW);
-	analogWrite(PWMB,190);
-}
-
-void applyBrakes ()
-{
-	digitalWrite (AIN1,HIGH);
-	digitalWrite (AIN2,HIGH);
-	analogWrite(PWMA,255);
-	digitalWrite (BIN1,HIGH);
-	digitalWrite (BIN2,HIGH);
-	analogWrite(PWMB,255);
-}
-
-
-void turnAround()
-{
-	rotateLeft();
-	delay(1370);
-}
-
-
-
 void setup() {
 
 	/* Define all 7 pins as outputs to the TB6612FNG driver */
@@ -101,6 +13,7 @@ void setup() {
 	pinMode(BIN2,OUTPUT);
 
 }
+
 
 void loop() {
 
--- test_motors.h
+++ test_motors.h
@@ -7,15 +7,104 @@
 #ifndef _test_motors_H_
 #define _test_motors_H_
 #include "Arduino.h"
-//add your includes for the project test_motors here
 
+/* Define pinout of Teensy to match physical connections */
 
-//end of add your includes here
+#define PWMA 20
+#define AIN1 11
+#define AIN2 12
+#define PWMB 21
+#define BIN1 15
+#define BIN2 14
+// STBY not defined, always at high state.
+
+void goForward ();
+void goBackward ();
+void rotateRight ();
+void rotateLeft ();
+void veerLeft ();
+void veerRight ();
+void applyBrakes ();
+void turnAround();
 
 
 //add your function definitions for the project test_motors here
+void goForward ()
+{
+	digitalWrite (AIN1,HIGH);
+	digitalWrite (AIN2,LOW);
+	analogWrite(PWMA,234);
+	digitalWrite (BIN1,HIGH);
+	digitalWrite (BIN2,LOW);
+	analogWrite(PWMB,255);
+}
+
+void goBackward ()
+{
+	digitalWrite (AIN1,LOW);
+	digitalWrite (AIN2,HIGH);
+	analogWrite(PWMA,233);
+	digitalWrite (BIN1,LOW);
+	digitalWrite (BIN2,HIGH);
+	analogWrite(PWMB,255);
+}
+
+void rotateRight ()
+{
+	digitalWrite (AIN1,HIGH);
+	digitalWrite (AIN2,LOW);
+	analogWrite(PWMA,255);
+	digitalWrite (BIN1,LOW);
+	digitalWrite (BIN2,HIGH);
+	analogWrite(PWMB,255);
+}
+
+void rotateLeft ()
+{
+	digitalWrite (AIN1,LOW);
+	digitalWrite (AIN2,HIGH);
+	analogWrite(PWMA,255);
+	digitalWrite (BIN1,HIGH);
+	digitalWrite (BIN2,LOW);
+	analogWrite(PWMB,255);
+}
+
+void veerLeft ()
+{
+	digitalWrite (AIN1,HIGH);
+	digitalWrite (AIN2,LOW);
+	analogWrite(PWMA,190);
+	digitalWrite (BIN1,HIGH);
+	digitalWrite (BIN2,LOW);
+	analogWrite(PWMB,255);
+}
+
+void veerRight ()
+{
+	digitalWrite (AIN1,HIGH);
+	digitalWrite (AIN2,LOW);
+	analogWrite(PWMA,255);
+	digitalWrite (BIN1,HIGH);
+	digitalWrite (BIN2,LOW);
+	analogWrite(PWMB,190);
+}
+
+void applyBrakes ()
+{
+	digitalWrite (AIN1,HIGH);
+	digitalWrite (AIN2,HIGH);
+	analogWrite(PWMA,255);
+	digitalWrite (BIN1,HIGH);
+	digitalWrite (BIN2,HIGH);
+	analogWrite(PWMB,255);
+}
 
 
+void turnAround()
+{
+	rotateLeft();
+	delay(1370);
+}
 
 
 //Do not add code below this line
